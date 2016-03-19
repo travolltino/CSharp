8e9bc4e9ad56bc953de4c5563bd97725da180a30
travolltino <uppdate@abv.bg>
2016-03-19 14:48:47 +0200

    Added files via upload

---
+++ CheckThirdNumberIsSeven.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckThirdNumberIsSeven
+{
+    class CheckThirdNumberIsSeven
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            int lenght = input.Length;
+            int other;
+            other = lenght - 1;
+            char checkedo = '7';
+
+            char[] result = new Char[lenght];
+            char[] proba = input.ToCharArray();
+            
+            for ( int i = 0; i< lenght ; i++)
+            {
+                result[i] = proba[other];
+                other -= 1;
+            }
+            bool itsTrue = (result[2] == checkedo);
+            Console.WriteLine("The third number is 7: {0}", itsTrue);
+
+        }
+    }
+}
---
+++ Equal5Or7.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equal5Or7
+{
+    class Equal5Or7
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            bool check = (input % 5 == 0) && (input % 7 == 0);
+            Console.WriteLine("The number {0} euqal for 7 and 5: {1}", input, check);
+        }
+    }
+}
