4b37581a897e5598c75b3c97470f19df8402894a
travolltino <uppdate@abv.bg>
2016-03-19 14:48:05 +0200

    Added files via upload

---
+++ EvenOrNo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrNo
+{
+    class EvenOrNo
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            bool check = (input % 2 == 0);
+            Console.WriteLine("The number {0} is even: {1}", input, check);
+        }
+    }
+}
